3dfc6b4de4b66e27f1ba4b66ae6d694935eacf71
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-10-19 03:08:07 -0700

    update module newsletters

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,11 @@
+Naturesoft::Nsmodule.class_eval do
+  @newsletters = {
+    "subscriber" => {
+      "label" => "Subscriber",
+      "options" => {
+        "description" => nil,
+        "style" => "default"
+      }    
+    },
+  }
+end
--- app/models/naturesoft/newsletters/newsletter.rb
+++ app/models/naturesoft/newsletters/newsletter.rb
@@ -2,6 +2,7 @@ module Naturesoft::Newsletters
   class Newsletter < ApplicationRecord
     
     validates :email, :presence => true, :uniqueness => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
     
     def self.sort_by
       [
